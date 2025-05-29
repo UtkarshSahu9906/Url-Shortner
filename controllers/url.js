54bbea51a346f66beb62f5c36a1654fea5643268
UtkarshSahu9906 <utkarshsahu9906@gmail.com>
2025-05-30 01:31:53 +0530

    form work done

--- controllers/url.js
+++ controllers/url.js
@@ -1,41 +1,49 @@
 const URL = require("../models/url");
-const shortid =require("shortid");
+const shortid = require("shortid");
 
-async function handleGenrateNewShortURL(req,res) {
-const body =req.body;
-if(!body.url){return res.status(404).json({err:'URL is require'})}
-const shortID = shortid();
-await URL.create({
+async function handleGenrateNewShortURL(req, res) {
+  const body = req.body;
+  if (!body.url) {
+    return res.status(404).json({ err: "URL is require" });
+  }
+  const shortID = shortid();
+  await URL.create({
     shortId: shortID,
-    redirectURL:body.url,
-    visitHistory:[],
-});
+    redirectURL: body.url,
+    visitHistory: [],
+  });
+  return res.render("home",{ id: shortID,newUrl: `http://localhost:8001/${shortID}`})
 
-return res.json({id:shortID});
-
+ // return res.json({ id: shortID,newUrl: `http://localhost:8001/${shortID}`});
 }
 
-async function handleGivenId(req,res) {
-    const ShortId = req.params.shortId;
-   const entry =  await URL.findOneAndUpdate({shortId:ShortId},{$push:{visitHistory:{timestamp:Date.now()}}});
-console.log(entry);
- res.redirect(entry.redirectURL);
-    
+async function handleGivenId(req, res) {
+  const shortId = req.params.shortId;
+  const entry = await URL.findOneAndUpdate(
+    { shortId: shortId },
+    { $push: { visitHistory: { timestamp: Date.now() } } }
+  );
+  console.log(entry);
+  res.redirect(entry.redirectURL);
 }
-async function handleShowAll(req,res) {
-   const data =  await URL.find({})
-console.log(data);
- return res.status(200).json(data);
-    
+async function handleShowAll(req, res) {
+  const data = await URL.find({});
+  console.log(data);
+  return res.status(200).json(data);
 }
 
-async function handleGetAnalytics(req,res) {
-    const shortId = req.params.shortId;
-    const result =  await URL.findOne({shortId});
- console.log(result);
-return res.json({
+async function handleGetAnalytics(req, res) {
+  const shortId = req.params.shortId;
+  const result = await URL.findOne({ shortId });
+  console.log(result);
+  return res.json({
     totalClicks: result.visitHistory.length,
-    analytics:result.visitHistory
-})     
- }
-module.exports ={handleGenrateNewShortURL,handleGivenId,handleShowAll,handleGetAnalytics};
+    analytics: result.visitHistory,
+  });
+}
+module.exports = {
+  handleGenrateNewShortURL,
+  handleGivenId,
+  handleShowAll,
+  handleGetAnalytics,
+};
--- index.js
+++ index.js
@@ -1,22 +1,43 @@
 const express = require("express");
 const app = express();
-const urlRoute = require('./routes/url');
-const {handleGivenId} = require('./controllers/url');
-const{connectMongoDB} = require("./connection");
+const urlRoute = require("./routes/url");
+const staticRoute = require("./routes/staticRouter");
 
+const { handleGivenId } = require("./controllers/url");
+const { connectMongoDB } = require("./connection");
+const path = require("path");
+const Url = require('./models/url')
 
-connectMongoDB("mongodb://127.0.0.1:27017/short-url").then(()=>{console.log("mongodb Connected!")
+connectMongoDB("mongodb://127.0.0.1:27017/short-url").then(() => {
+  console.log("mongodb Connected!");
 });
+// View Engine Setup
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
 
 app.use(express.json());
-app.use('/url', urlRoute);
-app.get('/:shortId',handleGivenId);
+app.use(express.urlencoded({extended:false}));
 
 
+app.use("/url", urlRoute);
+app.use("/",staticRoute);
+app.get("/:shortId", handleGivenId);
+
+// Test Routes
+app.get("/url/test", async (req, res) => {
+    const allUrls = await Url.find({});
+  return  res.render('home',{
+        urls:allUrls,
+    });
+
+  });
+
+ 
 
 
+  app.get("/", (req, res) => {
+    return res.json({ message: "URL Shortener API is running" }); // Better welcome message
+  });
 
 const PORT = 8001;
-app.listen(PORT,()=>console.log(`Server Started at Port ${PORT}`));
-
-
+app.listen(PORT, () => console.log(`Server Started at Port ${PORT}`));
